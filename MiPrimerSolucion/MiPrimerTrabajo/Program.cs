e87e32377a8ad9edfaf77e54a48470d7d43def84
alfredo1520 <alfredojaime.1520@gmail.com>
2020-01-30 14:28:19 -0600

    hola

---
+++ MiPrimerSolucion/MiPrimerTrabajo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerTrabajo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            Console.Write("introduce un numero: ");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("introduce un numero: ");
+            B = Convert.ToInt32(Console.ReadLine());
+            C = A + B;
+
+            Console.WriteLine("la suma de los numeros es: " + C);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
